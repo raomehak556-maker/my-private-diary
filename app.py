6b0d3e85b482927b838f18d4067d1f6b14f4c45b
raomehak556-maker <raomehak556@gmail.com>
2026-09-11 06:44:34 -0700

    Update app.py
    connect log in page

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>My Private Diary</h1><p>Welcome to your diary!</p>"
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        return "Login received for: " + email
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
